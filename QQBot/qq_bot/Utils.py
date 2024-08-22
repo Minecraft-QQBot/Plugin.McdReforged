35109268d5faed04169f795e50e5381822e0eb61
LonelySail <xiaocaicai_email@sina.com>
2024-08-22 17:49:10 +0800

    更新：修改一些小问题。

--- QQBot/qq_bot/Utils.py
+++ QQBot/qq_bot/Utils.py
@@ -5,7 +5,7 @@ from json import dumps, loads
 
 def encode(data: dict):
     # 编码
-    string = dumps(data)
+    string = dumps(data, ensure_ascii=False)
     string = string.encode('Utf-8')
     string = b64encode(string)
     return string.decode('Utf-8')
